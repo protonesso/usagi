c0c0f939d1b9cf5a5c14c680ffc23411e332b9ee
Ishimoto Shinobu <nagakamira@gmail.com>
2020-03-13 01:04:48 +0900

    argument parser
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- main.c
+++ main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "ext/argp/argp.h"
 
-int main() {
-	printf("Hello, world!\n");
+const char *argp_program_version = "usagi 0.1";
+const char *argp_program_bug_address = "<ataraxialinux@protonmail.com>";
+static char doc[] = "Lighweight package manager";
+static char args_doc[] = "[PACKAGE]...";
+static struct argp_option options[] = {
+	{ "install", 'i', 0, 0, "Install binary usagi/neko package"},
+	{ 0 } 
+};
+
+struct arguments {
+	char *args[8];
+	int silent, verbose;
+	char *output_file;
+};
+
+static int usg_install() {
+	printf("Installing!\n");
+	return 0;
+}
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
+	switch (key) {
+		case 'i':
+			usg_install();
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
+
+int main(int argc, char **argv) {
+	struct arguments arguments;
+
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
 	return 0;
 }
