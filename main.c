87b60735c4dd14118aa3880bf937bcf3e605fe38
Ishimoto Shinobu <nagakamira@gmail.com>
2020-06-25 23:12:43 +0900

    reduce loc
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- main.c
+++ main.c
@@ -3,10 +3,8 @@
 #include <string.h>
 
 static void usage() {
-	printf("usagi - Simple package manager\n");
-	printf("\n");
-	printf("Usage: usagi [ACTION] [OPTIONS] [PACKAGE]");
-	printf("\n");
+	printf("usagi - Simple package manager\n\n");
+	printf("Usage: usagi [ACTION] [OPTIONS] [PACKAGE]\n\n");
 	printf("Actions:\n");
 	printf("   build		Build package from source\n");
 	printf("   emerge		Build package and install it\n");
@@ -17,13 +15,10 @@ static void usage() {
 	printf("   search		Search for packages\n");
 	printf("   sync			Synchronize repositories\n");
 	printf("   upgrade		Upgrade all of installed packages\n");
-	printf("   version		Show usagi version\n");
-	printf("\n");
+	printf("   version		Show usagi version\n\n");
 	printf("Options:\n");
-	printf("In dev!\n");
-	printf("\n");
+	printf("In dev!\n\n");
 	printf("BUG TRACKER: https://github.com/protonesso/usagi/issues\n");
-	printf("\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -74,43 +69,21 @@ int main(int argc, char* argv[]) {
 
 		for (int i = 2; i < argc; i++) {
 			switch (mode) {
-				case 1:
-					printf("Emerging package: %s\n", argv[i]);
-					break;
-				case 2:
-					printf("Information for %s\n", argv[i]);
-					break;
-				case 3:
-					printf("Installing package: %s\n", argv[i]);
-					break;
-				case 5:
-					printf("Removing package: %s\n", argv[i]);
-					break;
-				case 6:
-					printf("Searching for package %s\n", argv[i]);
-					break;
-				default:
-					usage();
-					return 1;
+				case 1: printf("Emerging package: %s\n", argv[i]); break;
+				case 2: printf("Information for %s\n", argv[i]); break;
+				case 3: printf("Installing package: %s\n", argv[i]); break;
+				case 5: printf("Removing package: %s\n", argv[i]); break;
+				case 6: printf("Searching for package %s\n", argv[i]); break;
+				default: usage(); return 1;
 			}
 		}
 	} else {
 		switch (mode) {
-			case 0:
-				printf("Building package\n");
-				break;
-			case 4:
-				printf("Listing packages\n");
-				break;
-			case 7:
-				printf("Syncing repos\n");
-				break;
-			case 8:
-				printf("Upgrading system\n");
-				break;
-			default:
-				usage();
-				return 1;
+			case 0: printf("Building package\n"); break;
+			case 4: printf("Listing packages\n"); break;
+			case 7: printf("Syncing repos\n"); break;
+			case 8: printf("Upgrading system\n"); break;
+			default: usage(); return 1;
 		}
 	}
 
