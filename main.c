60e0add4a3e75f18b79dcf31194feca0dae8fa4e
Ishimoto Shinobu <nagakamira@gmail.com>
2020-10-04 14:14:28 +0300

    better

--- main.c
+++ main.c
@@ -3,22 +3,27 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "pm.h"
+
 static void usage() {
-	printf("usagi - Simple package manager\n\n");
-	printf("Usage: usagi [ACTION] [OPTIONS] [PACKAGE]\n\n");
+	printf("PIPE - Simple package manager\n\n");
+	printf("Usage: pipe [ACTION] [OPTIONS] [PACKAGE]\n\n");
 	printf("Actions:\n");
-	printf("   build		Build package from source\n");
-	printf("   emerge		Build package and install it\n");
+	printf("   garbage		Garbage collector\n");
 	printf("   info			Show information about package\n");
 	printf("   install		Install package from file\n");
 	printf("   list			List all installed packages\n");
 	printf("   remove		Remove installed package\n");
-	printf("   search		Search for packages\n");
-	printf("   sync			Synchronize repositories\n");
-	printf("   upgrade		Upgrade all of installed packages\n");
+	printf("   rollback		Rollback the system\n");
 	printf("   version		Show usagi version\n\n");
-	printf("Options:\n");
-	printf("In dev!\n\n");
+	printf("Options for package management:\n");
+	printf("   -c [FILE]		Specify configuration file for PIPE\n");
+	printf("   -r [PATH]		Specify root filesystem path for PIPE\n");
+	printf("   -f			Force installation\n");
+	printf("   -n			Do not reinstall the package\n");
+	printf("   -d			Do not check for dependencies and conflicts\n");
+	printf("   -s			Do not run scripts\n");
+	printf("   -t			Do not run triggers\n\n");
 	printf("BUG TRACKER: https://github.com/protonesso/usagi/issues\n\n");
 }
 
@@ -27,30 +32,25 @@ int main(int argc, char* argv[]) {
 	char* subarg = argv[1];
 	unsigned char mode;
 	int c;
+	struct options opts;
 
 	if (argc == 1) {
 		fprintf(stderr, "Command was not specified\n");
 		return 1;
 	}
 
-	if (strcmp(subarg, "build") == 0) {
-		mode = 0;
-	} else if (strcmp(subarg, "emerge") == 0) {
-		mode = 1; pkgarr = true;
+	if (strcmp(subarg, "gargbage") == 0) {
+		mode = 0; pkgarr = false;
 	} else if (strcmp(subarg, "info") == 0) {
-		mode = 2; pkgarr = true;
+		mode = 1; pkgarr = true;
 	} else if (strcmp(subarg, "install") == 0) {
-		mode = 3; pkgarr = true;
+		mode = 2; pkgarr = true;
 	} else if (strcmp(subarg, "list") == 0) {
-		mode = 4;
+		mode = 3; pkgarr = false;
 	} else if (strcmp(subarg, "remove") == 0) {
-		mode = 5; pkgarr = true;
-	} else if (strcmp(subarg, "search") == 0) {
-		mode = 6; pkgarr = true;
-	} else if (strcmp(subarg, "sync") == 0) {
-		mode = 7;
-	} else if (strcmp(subarg, "upgrade") == 0) {
-		mode = 8;
+		mode = 4; pkgarr = true;
+	} else if (strcmp(subarg, "rollback") == 0) {
+		mode = 5; pkgarr = false;
 	} else if (strcmp(subarg, "version") == 0) {
 		printf("Version is unknown\n");
 	} else {
@@ -60,18 +60,15 @@ int main(int argc, char* argv[]) {
 
 	while ((c = getopt(argc, argv, "c:r:")) != -1) {
 		switch (c) {
-			case 'c':
-				printf("Config file is %s\n", optarg);
-				break;
-			case 'r':
-				printf("Rootfs is %s\n", optarg);
-				break;
-			case ':':
-				fprintf(stderr, "Option needs a value\n");
-				break;
-			case '?':
-				fprintf(stderr, "Unknown option: %c\n", optopt);
-				break;
+			case 'c': printf("Config file is %s\n", optarg); break;
+			case 'r': printf("Rootfs is %s\n", optarg); break;
+			case 'f': opts.force = true; break;
+			case 'n': opts.needed = true; break;
+			case 'd': opts.nodeps = true; break;
+			case 's': opts.noscripts = true; break;
+			case 't': opts.notriggers = true; break;
+			case ':': fprintf(stderr, "Option needs a value\n"); break;
+			case '?': fprintf(stderr, "Unknown option: %c\n", optopt); break;
 		}
 	}
 
@@ -83,20 +80,17 @@ int main(int argc, char* argv[]) {
 
 		for (int i = optind + 1; i < argc; i++) {
 			switch (mode) {
-				case 1: printf("Emerging package: %s\n", argv[i]); break;
-				case 2: printf("Information for %s\n", argv[i]); break;
-				case 3: printf("Installing package: %s\n", argv[i]); break;
-				case 5: printf("Removing package: %s\n", argv[i]); break;
-				case 6: printf("Searching for package %s\n", argv[i]); break;
+				case 1: pipe_info(argv[i]); break;
+				case 2: pipe_install(argv[i]); break;
+				case 4: pipe_remove(argv[i]); break;
 				default: usage(); return 1;
 			}
 		}
 	} else {
 		switch (mode) {
-			case 0: printf("Building package\n"); break;
-			case 4: printf("Listing packages\n"); break;
-			case 7: printf("Syncing repos\n"); break;
-			case 8: printf("Upgrading system\n"); break;
+			case 0: pipe_garbage(); break;
+			case 3: pipe_listpkg(); break;
+			case 5: pipe_rollback(); break;
 			default: usage(); return 1;
 		}
 	}
---
+++ pm.h
@@ -0,0 +1,13 @@
+#ifndef PM_H
+#define PM_H
+
+#include <stdbool.h>
+
+struct options {
+	bool force;
+	bool needed;
+	bool nodeps;
+	bool noscripts;
+	bool notriggers;
+};
+#endif
