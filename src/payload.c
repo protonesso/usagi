b7066538be1d46bc7cc6c7de0dc54e7ac6b5ea4f
Ishimoto Shinobu <nagakamira@gmail.com>
2020-11-07 13:31:01 +0300

    extension

--- src/payload.c
+++ src/payload.c
@@ -16,7 +16,7 @@ int usagi_write_payloadHeader(const char *file) {
 	struct payloadHeader payload;
 
 	memset(&payload, 0, sizeof(struct payloadHeader));
-	snprintf(payload.extension, sizeof(payload.extension), "%s", ".USAGI");
+	snprintf(payload.extension, sizeof(payload.extension), "%s", ".UPK");
 	snprintf(payload.magic, sizeof(payload.magic), "%ld", 0x6174617261786961);
 	snprintf(payload.compress, sizeof(payload.compress), "%d", 1);
 	snprintf(payload.version, sizeof(payload.version), "%f", 1.0);
